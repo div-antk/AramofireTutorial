64b2342e2e8e19bdf2d20f6b5e5df9f82cf17862
div-antk <51283468+owaii@users.noreply.github.com>
2021-03-05 07:09:19 +0900

    Initial Commit

---
+++ AramofireTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AramofireTutorial
+//
+//  Created by Takuya Ando on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
